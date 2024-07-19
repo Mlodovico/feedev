449915acf4c9e4eb62484fe358e627c6b0e42213
mlodovico <murilo.lodovico@outlook.com>
2024-07-19 12:33:44 -0300

    feature: login page and structure

---
+++ src/app/login/page.tsx
@@ -0,0 +1,10 @@
+export default function Login() {
+  return (
+    <div className="flex min-h-screen items-center justify-center">
+      <div className="flex bg-gray-800 p-8 rounded-lg">
+        <h1>Login</h1>
+      </div>
+    </div>
+  );
+}
+217;
