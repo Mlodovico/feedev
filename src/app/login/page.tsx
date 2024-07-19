28e6b315db0ba565ef8ba30e73dfa61df660c88a
mlodovico <murilo.lodovico@outlook.com>
2024-07-19 12:57:39 -0300

    feature: centering styles

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,16 +2,16 @@ import Tag3d from "../components/tag";
 
 export default function Login() {
   return (
-    <>
-      <div className="h-svh w-svw">
+    <div className="relative w-full h-full">
+      <div className="absolute inset-0 opacity-50">
         <Tag3d />
       </div>
-      <div className=" posision-absolute top-0 left-0 flex items-center justify-center h-svh w-svw">
+      <div className=" relative top-0 left-0 flex items-center justify-center h-svh w-svw">
         <div className="flex bg-gray-800 p-8 rounded-lg">
           <h1>Login</h1>
         </div>
       </div>
-    </>
+    </div>
   );
 }
 217;
