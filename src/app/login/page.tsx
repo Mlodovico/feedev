9aa4c1f652c5eb52fa0b94172e270d872c9d3c2e
mlodovico <murilo.lodovico@outlook.com>
2024-07-19 12:52:24 -0300

    feature: implementing eldora ui

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,10 +1,17 @@
+import Tag3d from "../components/tag";
+
 export default function Login() {
   return (
-    <div className="flex min-h-screen items-center justify-center">
-      <div className="flex bg-gray-800 p-8 rounded-lg">
-        <h1>Login</h1>
+    <>
+      <div className="h-svh w-svw">
+        <Tag3d />
       </div>
-    </div>
+      <div className=" posision-absolute top-0 left-0 flex items-center justify-center h-svh w-svw">
+        <div className="flex bg-gray-800 p-8 rounded-lg">
+          <h1>Login</h1>
+        </div>
+      </div>
+    </>
   );
 }
 217;
